e0f92ddcca4be7d4322bfaef327d9df3040253c3
Maxime Tribolet <maxime.tribolet@gmail.com>
2022-11-23 15:33:22 +0100

    conflict resolved

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,10 @@ class User < ApplicationRecord
   has_one :user_game
   has_many :bookings
 
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
-  # validates :address, presence: true
-  # validates :phone_number, presence: true
-  # validates :plz, presence: true
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :plz, presence: true
 end
