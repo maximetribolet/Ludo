034c51f4608a07a7514de08f201754f69136574a
Maxime Tribolet <maxime.tribolet@gmail.com>
2022-11-23 15:35:14 +0100

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,7 @@ class User < ApplicationRecord
 
   has_one :user_game
   has_many :bookings
-
-
+  
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :address, presence: true
