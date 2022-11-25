938c5a041c1d3585311f3ea807e630a57a25a98b
Raphael Ventura <raphaelventura55@gmail.com>
2022-11-25 12:17:52 +0100

    booking buttons

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     resources :user_ratings, only: %i[create new show index]
     resources :booking, only: %i[show index]
   end
-  resources :bookings, only: %i[destroy]
+  resources :bookings, only: %i[destroy, update]
   resources :user_ratings, only: %i[destroy]
 
   get "/my_games", to: "user_games#mine", as: :mine
